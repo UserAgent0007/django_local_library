341f56389c88d2777b1a0bda1b5d9a6a796374ba
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-07-19 20:01:28 +0300

    р

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -46,11 +46,11 @@ ALLOWED_HOSTS = ['web-production-cabf3.up.railway.app' ,'127.0.0.1']
 #     'http://127.0.0.1:8000',
 # ]
 
-trusted_origins = os.environ.get('CSRF_TRUSTED_ORIGINS')
-if trusted_origins:
-    CSRF_TRUSTED_ORIGINS = trusted_origins.split(',')
-else:
-    CSRF_TRUSTED_ORIGINS = []
+# trusted_origins = os.environ.get('CSRF_TRUSTED_ORIGINS')
+# if trusted_origins:
+#     CSRF_TRUSTED_ORIGINS = trusted_origins.split(',')
+# else:
+#     CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
