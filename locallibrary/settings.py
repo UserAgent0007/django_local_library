0a4be6d64417becb3b0f786a774a40eba4b4afd0
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-07-19 20:28:18 +0300

    sd

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-cabf3.up.railway.app' ,'127.0.0.1']
+ALLOWED_HOSTS = ['https://web-production-cabf3.up.railway.app' ,'127.0.0.1']
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'https://*.railway.app',
