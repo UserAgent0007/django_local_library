132ce799d66c9e2e5922f668b0eccebf3e599b76
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-07-19 18:31:25 +0300

    add CSRF_TRUSTED_ORIGINS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -36,8 +36,14 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-cabf3.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-cabf3.up.railway.app','127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-cabf3.up.railway.app',
+    # Також, якщо розробляєте локально:
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 # Application definition
 
